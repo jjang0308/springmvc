0e9394cdbc3ee9ccdb61dd525c7115e075a1c119
jjang0308 <jang653838@naver.com>
2024-03-19 11:01:58 +0900

    commit

--- src/main/java/hello/springmvc/SpringmvcApplication.java
+++ src/main/java/hello/springmvc/SpringmvcApplication.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Controller;
 @SpringBootApplication
 public class SpringmvcApplication {
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringmvcApplication.class, args);
 	}
